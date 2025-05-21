274e3542901cbe3d66dcdb8460542d41cc0e5098
Nahu Caroseli <nahucaroseli2100@gmail.com>
2025-05-21 14:19:06 -0300

    agregado avisos al iniciar sesion y registrarse

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -10,9 +10,13 @@ function Authentication() {
     const [password, setPass] = useState("");
     const [usernameRegister, setUserNameRegister] = useState("");
     const [passwordRegister, setPassRegister] = useState("");
-
-
+    const [registerMessage, setRegisterMessage] = useState(false);
+    const [loginMessage, setLoginMessage] = useState(false);
+    const [loginErrorMessage, setLoginErrorMessage] = useState(false);
     const navigate = useNavigate();
+
+
+
 
 
     const submitRegister = async()=>{
@@ -20,7 +24,7 @@ function Authentication() {
         username:usernameRegister,
         password:passwordRegister
       }
-
+      setRegisterMessage(true);
       const user = await signIn(userRegistered);
       const newUser = JSON.parse(JSON.stringify(user));
       if(newUser.username == userRegistered.username){
@@ -29,6 +33,7 @@ function Authentication() {
         setNewUser();
         setUsers([...users,newUser])
         navigate("/")
+        setTimeout(()=>setRegisterMessage(false),3000);
       }
 
     }
@@ -39,14 +44,24 @@ function Authentication() {
           username:username,
           password:password
          }
+         setLoginMessage(true)
          const user = await logIn(userLogin);
+         console.log(user.status)
+         if(user.status === 500){
+
+            setLoginMessage(false);
+            setLoginErrorMessage(true);
+            setTimeout(()=>setLoginErrorMessage(false),3000);
+          }
          const userJson = JSON.parse(JSON.stringify(user));
          if(userJson.username == userLogin.username){
             localStorage.setItem("username",username);
             localStorage.setItem("user_id",userJson.id)
             setNewUser();
+            setTimeout(()=>setLoginMessage(false),3000);
             navigate("/");
          }
+
     }
 
 
@@ -61,6 +76,12 @@ function Authentication() {
                   <input value={username} onChange={(e)=>{setUserName(e.target.value)}} className='border border-black p-2' type="text" required={true}/>
                   <h1>Contraseña</h1>
                   <input value={password} onChange={(e)=>{setPass(e.target.value)}} className='border border-black p-2' type="password" required={true}/><button className=' mt-10 border-2 border-black p-2 rounded-lg w-60 text-white bg-black hover:bg-white hover:text-black ease-out duration-300 '>Acceder</button>
+                  {loginMessage == true &&
+                    <p className='text-green-700'>Iniciando Sesion...</p>
+                  }
+                  {loginErrorMessage == true &&
+                    <p className='text-red-700'>Usuario o contraseña incorrectos</p>
+                  }
               </form>
             </div>
             <div className='bg-black w-5'>
@@ -70,10 +91,14 @@ function Authentication() {
               <h1 className='text-xl font-bold'>Registrarse</h1>
               <form onSubmit={(e)=> { e.preventDefault(); submitRegister();}} className='flex flex-col p-10 w-11/12 h-100 gap-y-3 rounded-xl border-2 border-black md:w-full'>
                   <h1>Nombre de Usuario</h1>
-                  <input  value={usernameRegister} onChange={(e)=>{setUserNameRegister(e.target.value)}} className='border border-black p-2' type="text"/>
+                  <input  value={usernameRegister} onChange={(e)=>{setUserNameRegister(e.target.value)}} className='border border-black p-2' type="text"  required={true}/>
                   <h1>Contraseña</h1>
-                  <input value={passwordRegister} onChange={(e)=>{setPassRegister(e.target.value)}} className='border border-black p-2' type="password"/><button type='submit' className=' mt-10 border-2 border-black p-2 rounded-lg w-60 text-white bg-black hover:bg-white hover:text-black ease-out duration-300 '>Registrarse</button>
-              </form>
+
+                  <input value={passwordRegister} onChange={(e)=>{setPassRegister(e.target.value)}} className='border border-black p-2' type="password" required={true}/><button type='submit' className=' mt-10 border-2 border-black p-2 rounded-lg w-60 text-white bg-black hover:bg-white hover:text-black ease-out duration-300 '>Registrarse</button>
+                  {registerMessage == true &&
+                    <p className='text-green-700'>Registrandose...</p>
+                  }
+              </form> 
             </div>
 
           </div>
